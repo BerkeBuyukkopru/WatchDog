68eda0c3c8eaa6174f195d2517d6259561ce926a
Berke Büyükköprü <berkebuyukkopru06@gmail.com>
2026-02-26 11:33:49 +0300

    Infra ve modules arayüzleri eklendi

---
+++ modules/HealthChecks.Redis/RedisHealthCheck.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthChecks.Redis
+{
+    internal class RedisHealthCheck
+    {
+    }
+}
---
+++ src/Infrastructure/Watchdog.Infrastructure/Notifications/MailSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Watchdog.Infrastructure.Notifications
+{
+    internal class MailSender
+    {
+    }
+}
