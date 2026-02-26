53449a4140d51f02131096afc1215d7d57b8e8a9
Berke Büyükköprü <berkebuyukkopru06@gmail.com>
2026-02-26 15:13:41 +0300

    Enum eklendi. HealthCheks.Abstractions Yazıldı.

--- modules/HealthChecks.Abstractions/IHealthCheck.cs
+++ modules/HealthChecks.Abstractions/IHealthCheck.cs
@@ -6,5 +6,10 @@ namespace HealthChecks.Abstractions
 {
     internal interface IHealthCheck
     {
+        // Modülün ayırt edici adı (Örn: "SqlServer_Watchdog", "Main_API_Ping")
+        string Name { get; }
+
+        // Asıl sağlık kontrolü işlemini gerçekleştiren asenkron metot
+        Task<HealthCheckResult> CheckHealthAsync(CancellationToken cancellationToken = default);
     }
 }
